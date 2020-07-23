047a332956a6358b34a7fcaa23a2886ba3714726
nsaloua <naamasaloua1998@gmail.com>
2020-07-23 23:55:23 +0200

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,16 @@
 using Documenter
 
 
-makedocs()
+
+makedocs(
+modules = [ControleOptimal],
+    sitename = "ControleOptimal.jl",
+    strict=true,
+    authors = "Saloua Naama, Mohamed El Waghf et Rachid ELMontassir",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages = [
+            "Accueil" => "index.md"
+    ]
+    )
+
+deploydocs(repo = "github.com/mathn7/ControlOptimal.git")
